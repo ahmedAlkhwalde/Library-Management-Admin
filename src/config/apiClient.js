1164e74200eda5b7d07bcd9a5fa6e26992e2c5bf
ahmedAlkhwalde <ahmedkhkh224411@gmail.com>
2026-06-05 17:47:30 +0300

    add profile page and all api

--- src/config/apiClient.js
+++ src/config/apiClient.js
@@ -4,7 +4,7 @@ const apiClient = axios.create({
   baseURL: "http://192.168.1.4:8000/api",
   timeout: 10000,
   headers: {
-    "Content-Type": "application/json",
+    // "Content-Type": "application/json",
     Accept: "application/json",
   },
 });
